fc5ccc8efc4ddfdf367909827d531b08ecb94eb1
Oksana Klymenok <kapris15@yahoo.com>
2021-06-03 18:47:15 -1000

    changes

--- src/components/Converter/ConverterList.jsx
+++ src/components/Converter/ConverterList.jsx
@@ -5,43 +5,38 @@ import FormConverter from './FormConverter';
 
 const ConverterList = () => {
   let arrCrypto = useSelector((state) => state.crypto.cryptoArray);
-  console.log('hey', arrCrypto);
-const [selectedCrypto, setSelectedCrypto]=useState({})
+  const [selectedCrypto, setSelectedCrypto] = useState({});
 
-const clickedCard=(name)=>{
- const chosenCard= arrCrypto.find(el=>el.name===name);
- console.log(selectedCrypto)
-setSelectedCrypto(chosenCard)
-}
-console.log("selectedCrypto", selectedCrypto);
-
+  const clickedCard = (name) => {
+    const chosenCard = arrCrypto.find((el) => el.name === name);
+    setSelectedCrypto(chosenCard);
+  };
   return (
     <div className="container mt-5">
-
-<div className="row mt-5">
+      <div className="row mt-5">
         <div className="col-6 m-auto">
-        <FormConverter symbol={selectedCrypto.symbol} rate={selectedCrypto.current_price}/>
+          <FormConverter
+            symbol={selectedCrypto.symbol}
+            rate={selectedCrypto.current_price}
+          />
         </div>
-       
       </div>
-
       <div className="row">
         <div className="col">
           <div className="d-flex flex-wrap">
             {arrCrypto.map((el) => (
-              <Converter key={el.id}
+              <Converter
+                key={el.id}
                 icon={el.image}
                 title={el.name}
                 rate={el.current_price}
-                selected={el.name===selectedCrypto.name}
+                selected={el.name === selectedCrypto.name}
                 clickedCard={clickedCard}
               />
             ))}
           </div>
-          
         </div>
       </div>
-     
     </div>
   );
 };
